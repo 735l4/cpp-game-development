a4818fb906f6a237e7d133e307a108581c6e52ec
735l4 <pratibimbakhadka@gmail.com>
2025-04-20 15:52:08 +0545

    chore: reimplement collision detection for learning

--- collision/src/game.cpp
+++ collision/src/game.cpp
@@ -1,87 +1,61 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <vector>
-#include<memory>
+#include<SFML/Graphics.hpp>
+
 
 int main()
 {
-  float radius = 25.f;
-  uint wWidth = 800;
-  uint wHeight = 800;
-  float diameter = 2 * radius;
+  sf::RenderWindow window(sf::VideoMode({500, 500}), "SFML WINDOW");
+  sf::CircleShape c(20.f);
+  c.setPosition({200, 200});
 
-  sf::RenderWindow window(sf::VideoMode({wWidth, wHeight}), "Game Programming");
-  // sf::CircleShape circle(radius);
-  // sf::RectangleShape rect({100, 100});
-  //
-  // circle.setFillColor(sf::Color::Yellow);
-  // circle.setPosition({0.f, 0.f}); // Start from top-left
+  sf::Vector2f pos = sf::Vector2f({0.1f, 0.15f});
 
-  float movementSpeed = .05f;
+  c.setFillColor(sf::Color::Cyan);
 
-  float xSpeed = movementSpeed;
-  float ySpeed = movementSpeed;
+  window.setFramerateLimit(60.f);
 
-  while (window.isOpen())
+
+  while(window.isOpen())
   {
-    while (std::optional event = window.pollEvent())
+
+    while(const std::optional event = window.pollEvent())
     {
-      if(event->is<sf::Event::Closed>()) {
+      if(event->is<sf::Event::Closed>())
+      {
         window.close();
       }
     }
 
-    std::vector<std::shared_ptr<sf::Shape>> shapes;
-    std::shared_ptr<sf::Shape> shape = std::make_shared<sf::CircleShape>(20.f);
-    std::shared_ptr<sf::Shape> shape1 = std::make_shared<sf::CircleShape>(20.f);
-    std::shared_ptr<sf::Shape> rect = std::make_shared<sf::RectangleShape>(sf::Vector2f(40, 40));
-
-    shape1->setPosition({100.f, 100.f});
-    rect->setPosition({130.f, 140.f});
-
-    shapes.push_back(shape);
-    shapes.push_back(shape1);
-    shapes.push_back(rect);
-
-
-    //
-    // std::vector<sf::Shape*> shapes;
-    // sf::Shape* shape = new sf::CircleShape(20.f);
-    //
-    //
-    // shapes.push_back(shape);
-    // sh
-
-
-    for(auto& shape : shapes)
+    // Top
+    if(c.getPosition().y < 0)
     {
-      window.draw(*shape);
+      pos.y = -pos.y;
     }
 
+    //Left
+    if(c.getPosition().x < 0)
+    {
+      pos.x = -pos.x;
+    }
 
-    // Cool Trick
-    // std::vector<sf::RectangleShape> rectangles;
-    //
-    // for(float i =0; i<25; i++)
-    // {
-    //   for (float j = 0; j < 10; j++)
-    //   {
-    //     sf::RectangleShape rect(sf::Vector2f({15, 15}));
-    //     rect.setPosition({i * 20, j * 20});
-    //     rect.setFillColor(sf::Color(i * 10, j *10, 0));
-    //
-    //     rectangles.push_back(rect);
-    //
-    //   }
-    // }
+    // Bottom
+    if((c.getPosition().y + c.getLocalBounds().size.y) > window.getSize().y)
+    {
+      pos.y = -pos.y;
+    }
 
-    // for(auto& rect: rectangles)
-    // {
-    //   window.draw(rect);
-    // }
+    // Right
+    if((c.getPosition().x + c.getLocalBounds().size.x) > window.getSize().x)
+    {
+      pos.x = -pos.x;
+    }
+
+    c.move(pos);
+
+    window.clear(sf::Color::Black);
+
+    window.draw(c);
     window.display();
   }
 
   return 0;
 }
-
